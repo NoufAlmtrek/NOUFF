7d837b3f41db3712431dec522db74c26d62af60a
NoufAlmtrek <noufalmtrek@gmail.com>
2023-10-08 17:46:08 +0300

    Update

--- NOUFF/ContentView.swift
+++ NOUFF/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+     
+    
+        Image("NOUFFF PIC")
+            .frame(width: 100,height: 100)
+            .background(Color.pink)
+            .clipShape(Circle())
+            .padding(50)
+            .background(Circle().strokeBorder(Color.black, lineWidth: 10 ))
+        VStack(alignment: .leading) {
+            Text("Nouf").bold()
+                .font(.largeTitle
+                )
+                .foregroundStyle(.pink)
+            Text("i am a 21 year student at imamu my major is IS ")
+            
         }
-        .padding()
+        
+        
     }
 }
 
