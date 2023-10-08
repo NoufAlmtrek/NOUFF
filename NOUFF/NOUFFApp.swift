013dc82eaddce9fe59477818d9995c8c3551de58
Nouf <Noufalmtrek@gmail.com>
2023-10-08 16:02:55 +0300

    Initial Commit

---
+++ NOUFF/NOUFFApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOUFFApp.swift
+//  NOUFF
+//
+//  Created by Nouf Almtrek on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NOUFFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
