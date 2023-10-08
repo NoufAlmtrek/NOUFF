aa4f8ce4b6b0aab131509ed759d1683a03847373
LATIFARYES <latifaryes@gmail.com>
2023-10-08 18:00:33 +0300

    I ADD MY PHOTO LATIFA

---
+++ NOUFF/LATIFA.swift
@@ -0,0 +1,18 @@
+//
+//  LATIFA.swift
+//  NOUFF
+//
+//  Created by LATIFA on 08/10/2023.
+//
+
+import SwiftUI
+
+struct LATIFA: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LATIFA()
+}
